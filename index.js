dd20ef723ea34ebe9fbc82109e59ce9842c97011
Ballbot <ballbot@cashwalk.io>
2023-04-24 16:07:30 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,142 @@
+const DEPT_PATH = ''; // 정리해야할 사진들이 있는 폴더 경로
+const ARIV_PATH = ''; // 정리 된 사진들이 저장될 폴더 경로
+const UNKNOWN = ''; // 정리에 실패한 파일들은 어디로?
+
+const fs = require('fs');
+const {parse} = require("date-fns");
+const ExifReader = require('exifreader');
+const path = require("path");
+
+function getFilesFromDept() {
+  return fs.readdirSync(DEPT_PATH);
+}
+
+function isDateCorrect(date) {
+  if (!(date instanceof Date) || !isNaN(date)) {
+    return false;
+  }
+
+  if (date.getFullYear() < 2002 || date.valueOf() > new Date().valueOf()) {
+    // 02년 ~ 지금 사이의 사진이 아니면 제외
+    return false;
+  }
+
+  return true
+}
+
+function parseDateFromFileName(fileName) {
+  const numberOnlyFileName = fileName.replace(/\d+/g, '');
+  let date = null;
+
+  if (numberOnlyFileName.length === 13 || numberOnlyFileName.length === 10) {
+    // Epoch Timestamp 의 경우
+    date = new Date(parseInt(numberOnlyFileName, 10));
+  }
+
+  if (numberOnlyFileName.length === 14 && numberOnlyFileName.startsWith('20')) {
+    // YYYYMMDDHHMMSS 형태의 경우
+    date = parse(numberOnlyFileName, 'yyyyMMddHHmmss', null);
+  }
+
+  if (numberOnlyFileName.length === 12) {
+    // YYMMDDHHMMSS 형태의 경우
+    date = parse(numberOnlyFileName, 'yyMMddHHmmss', null);
+  }
+
+  return {
+    isCorrect: isDateCorrect(date),
+    date,
+  }
+}
+
+async function parseDateFromExif(filePath) {
+  const tags = await ExifReader.load(filePath);
+  let dateTimeString = null;
+
+  if (!tags) {
+    return {
+      isCorrect: false,
+      date: null,
+    }
+  }
+
+  if (tags.DateTime) {
+    if (tags.DateTime.value) {
+      dateTimeString = tags.DateTime.value;
+    }
+
+    if (tags.DateTime.description) {
+      dateTimeString = tags.DateTime.description;
+    }
+  }
+
+  if (tags.DateTimeOriginal) {
+    if (tags.DateTimeOriginal.value) {
+      dateTimeString = tags.DateTimeOriginal.value;
+    }
+
+    if (tags.DateTimeOriginal.description) {
+      dateTimeString = tags.DateTimeOriginal.description;
+    }
+  }
+
+  return parseDateFromFileName(dateTimeString);
+}
+
+function copyFile(fromFilePath, toFilePath) {
+  try {
+    fs.accessSync(toFilePath)
+    return false;
+  } catch (e) {}
+
+  const toFileFolder = path.dirname(toFilePath);
+  if (!fs.existsSync(toFileFolder)) {
+    fs.mkdirSync(toFileFolder, { recursive: true });
+  }
+
+  fs.copyFileSync(fromFilePath, toFilePath);
+}
+
+function generateFolderNameFromDate(date) {
+  return `${date.getFullYear()}. ${date.getMonth() + 1}`;
+}
+
+async function bootstrap() {
+  const deptFiles = getFilesFromDept();
+  const result = {
+    successByFileName: 0,
+    successByExif: 0,
+    failure: 0,
+  }
+
+  for (const deptFile of deptFiles) {
+    const fileName = deptFile.split('.')[0];
+    const dateParseResult = [
+      parseDateFromFileName(fileName),
+      await parseDateFromExif(fileName),
+    ]
+
+    if (dateParseResult[0].isCorrect) {
+      copyFile(
+        path.join(DEPT_PATH, deptFile),
+        path.join(ARIV_PATH, generateFolderNameFromDate(dateParseResult[0].date), deptFile));
+      result.successByFileName++;
+      break;
+    }
+
+    if (dateParseResult[1].isCorrect) {
+      copyFile(
+        path.join(DEPT_PATH, deptFile),
+        path.join(ARIV_PATH, generateFolderNameFromDate(dateParseResult[1].date), deptFile));
+      result.successByExif++;
+      break;
+    }
+
+    copyFile(
+      path.join(DEPT_PATH, deptFile),
+      path.join(ARIV_PATH, UNKNOWN, deptFile));
+    result.failure++;
+  }
+}
+
+bootstrap()
